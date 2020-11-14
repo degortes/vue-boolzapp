75a8fccc2756e474de6796b0995a407c26158a16
Danilo <danilodegortes@gmail.com>
2020-11-14 10:21:56 +0100

    eliminazione messaggi completata

--- main.js
+++ main.js
@@ -12,16 +12,19 @@ var app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                        eraser: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
                     },
                     {
+                        eraser: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di dargli da mangiare',
                         status: 'sent'
                     },
                     {
+                        eraser: false,
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received'
@@ -35,16 +38,19 @@ var app = new Vue ({
                 messages: [
                     {
                         date: '20/03/2020 16:30:00',
+                        eraser: false,
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
                         date: '20/03/2020 16:30:55',
+                        eraser: false,
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
                         date: '20/03/2020 16:35:00',
+                        eraser: false,
                         message: 'Mi piacerebbe ma devo andare a fare laspesa.',
                         status: 'received'
                     }
@@ -57,16 +63,19 @@ var app = new Vue ({
                 messages: [
                     {
                         date: '28/03/2020 10:10:40',
+                        eraser: false,
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
                         date: '28/03/2020 10:20:10',
+                        eraser: false,
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
                         date: '28/03/2020 16:15:22',
+                        eraser: false,
                         message: 'Ah scusa!',
                         status: 'received'
                     }
@@ -80,11 +89,13 @@ var app = new Vue ({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
+                        eraser: false,
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
+                        eraser: false,
                         status: 'received'
                     }
                 ]
@@ -93,34 +104,42 @@ var app = new Vue ({
     },
     methods: {
         takeIndex(index) {
-            app.myIndex = index;
+            this.myIndex = index;
         },
         insert(myIndex) {
-            app.contacts[myIndex].messages.push({message : app.reply, status : 'sent' })
-            app.reply = "";
+            this.contacts[myIndex].messages.push({message : this.reply, status : 'sent', eraser: false})
+            this.reply = "";
 
             var x = setTimeout(function() {
-                app.contacts[myIndex].messages.push({message : 'ok', status : 'received' });
+                app.contacts[myIndex].messages.push({message : 'ok', status : 'received', eraser: false });
             },1000);
         },
-        eraseMsg(item) {
-            item.eraser = !item.eraser;
-        }
-    },
-    beforeCreated: {
-        function() {
-            app.contacts.forEach((item, i) => {
-                item.messages.foreach((index, i) => {
-                    index.eraser = false;
-
+        sure(item) {
+            if (item.eraser) {
+                app.contacts.forEach((user, i) => {
+                    user.messages.forEach((msg, i) => {
+                        msg.eraser = false;
+                    });
                 });
 
-            })
+            }else {
+                app.contacts.forEach((user, i) => {
+                    user.messages.forEach((msg, i) => {
+                        msg.eraser = false;
+                    });
+                });
 
-
+                item.eraser = true;
+            }
+        },
+        elimina(myIndex,index) {
+            app.contacts[myIndex].messages.splice(index,1);
         }
 
     }
+
+
+
 
 
 
