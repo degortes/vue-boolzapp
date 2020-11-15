d318849ee4b49b1f3005e6493eb5031f1af74d9c
Danilo <danilodegortes@gmail.com>
2020-11-15 21:00:16 +0100

    aggiunto moment js. da stilare css

--- main.js
+++ main.js
@@ -1,16 +1,48 @@
 var app = new Vue ({
     el: '#root',
     data: {
-        deleting: false,
+
         myIndex: 0,
         reply: "",
         findUser: "",
         contacts: [
             {
+                name: 'Fabio',
+                avatar: '_2',
+                visible: true,
+                messages: [
+                    {
+                        hide: true
+                    },
+                    {
+                        eraser: false,
+                        date: '20/03/2020 16:30:00',
+                        message: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        eraser: false,
+                        date: '20/03/2020 16:30:55',
+                        message: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        eraser: false,
+                        date: '20/03/2020 17:30:55',
+                        message: 'Mi piacerebbe ma devo andare a fare laspesa.',
+                        status: 'received'
+                    }
+                ]
+            },
+            {
                 name: 'Michele',
                 avatar: '_1',
                 visible: true,
                 messages: [
+                    {
+                        hide: true
+                    },
+
                     {
                         eraser: false,
                         date: '10/01/2020 15:30:55',
@@ -29,32 +61,7 @@ var app = new Vue ({
                         message: 'Tutto fatto!',
                         status: 'received'
                     }
-                ],
-            },
-            {
-                name: 'Fabio',
-                avatar: '_2',
-                visible: true,
-                messages: [
-                    {
-                        date: '20/03/2020 16:30:00',
-                        eraser: false,
-                        message: 'Ciao come stai?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '20/03/2020 16:30:55',
-                        eraser: false,
-                        message: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
-                    },
-                    {
-                        date: '20/03/2020 16:35:00',
-                        eraser: false,
-                        message: 'Mi piacerebbe ma devo andare a fare laspesa.',
-                        status: 'received'
-                    }
-                ],
+                ]
             },
             {
                 name: 'Samuele',
@@ -62,30 +69,38 @@ var app = new Vue ({
                 visible: true,
                 messages: [
                     {
-                        date: '28/03/2020 10:10:40',
+                        hide: true
+                    },
+
+                    {
                         eraser: false,
+                        date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
-                        date: '28/03/2020 10:20:10',
                         eraser: false,
+                        date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
-                        date: '28/03/2020 16:15:22',
                         eraser: false,
+                        date: '28/03/2020 10:25:22',
                         message: 'Ah scusa!',
                         status: 'received'
                     }
-                ],
+                ]
             },
             {
                 name: 'Luisa',
                 avatar: '_4',
                 visible: true,
                 messages: [
+                    {
+                        hide: true
+                    },
+
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
@@ -107,11 +122,11 @@ var app = new Vue ({
             this.myIndex = index;
         },
         insert(myIndex) {
-            this.contacts[myIndex].messages.push({message : this.reply, status : 'sent', eraser: false})
+            this.contacts[myIndex].messages.push({message : this.reply, status : 'sent', eraser: false, date: moment().format()})
             this.reply = "";
 
             var x = setTimeout(function() {
-                app.contacts[myIndex].messages.push({message : 'ok', status : 'received', eraser: false });
+                app.contacts[myIndex].messages.push({message : 'ok', status : 'received', eraser: false, date: moment().format()});
             },1000);
         },
         sure(item) {
@@ -136,7 +151,27 @@ var app = new Vue ({
             app.contacts[myIndex].messages.splice(index,1);
         }
 
+
+    },
+    filters: {
+        moment1: function (master) {
+            if (master) {
+                return moment(master , 'DD-MM-YYYY, hh:mm:ss').format('DD-MMM');
+
+            }
+        },
+        moment2: function (master) {
+            if (master) {
+                return moment(master , 'DD-MM-YYYY, hh:mm:ss').format('hh:mm a');
+
+            }
+        }
+
     }
+
+
+
+
 
 
 
